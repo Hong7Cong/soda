583bfaa3eb90dd407519a38bca1e9caad7052e6c
Huy Nguyen <huynguyentranwe@gmail.com>
2024-07-08 16:40:52 -0700

    Only train glaucoma
    +Only train glaucoma instead of both glaucoma and normal. pretty hacky way to do this but for testing purpose.

--- datasets.py
+++ datasets.py
@@ -120,15 +120,18 @@ class FundusDataset(Dataset):
         classes = sorted(classes)
         assert len(classes) == 2
         self.data = []
+
         for idx, name in enumerate(classes):
             this_dir = os.path.join(self.train_dir, name)
             for root, _, files in sorted(os.walk(this_dir)):
                 for fname in sorted(files):
                     if fname.endswith(".jpg"):
-                        path = os.path.join(root, fname)
-                        item = (path, idx)
-                        self.data.append(item)
-        self.labels_dict = {i: classes[i] for i in range(len(classes))}
+                            if name == "glaucoma":
+                                path = os.path.join(root, fname)
+                                item = (path, idx)
+                                self.data.append(item)
+        # self.labels_dict = {i: classes[i] for i in range(len(classes))}
+        self.labels_dict = {0: 'glaucoma'}
         
     def __len__(self):
         return len(self.data)
@@ -164,7 +167,7 @@ def get_dataset(name='cifar10', root='data'):
         data_norm = transforms.Normalize([0.4802, 0.4481, 0.3975], [0.2302, 0.2265, 0.2262])
         NUM_CLASSES = 2
         DATASET = FundusDataset
-        RES = 32
+        RES = 128
     else:
         raise NotImplementedError
 
