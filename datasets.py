522308586f27dc7a731b3e1967a0b9d22920caa2
Huy Nguyen <huynguyentranwe@gmail.com>
2024-07-10 16:00:06 -0700

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -113,24 +113,46 @@ class FundusDataset(Dataset):
         self.train_dir = os.path.join(root, "train")
         self.val_dir = os.path.join(root, "val")
         self.transform = transform
-        self._scan_train_val()
-
-    def _scan_train_val(self):
+        if train:
+            self._scan_train()
+        else:
+            self._scan_val()
+            
+    def _scan_val(self):
+        self.file_to_class = {}
+        classes = [d.name for d in os.scandir(self.train_dir) if d.is_dir()]
+        classes = sorted(classes)
+        assert len(classes) == 2  
+        class_to_idx = {classes[i]: i for i in range(len(classes))}
+        self.data = []
+        for fname, class_name in self.file_to_class.items():
+            path = os.path.join(self.val_dir, class_name, fname)
+            idx = class_to_idx[class_name]
+            item = (path, idx)
+            self.data.append(item)
+            self.file_to_class[fname] = class_name
+        self.labels_dict = {i: classes[i] for i in range(len(classes))}
+        
+    def _scan_train(self):
         classes = [d.name for d in os.scandir(self.train_dir) if d.is_dir()]
         classes = sorted(classes)
         assert len(classes) == 2
         self.data = []
+
         for idx, name in enumerate(classes):
             this_dir = os.path.join(self.train_dir, name)
             for root, _, files in sorted(os.walk(this_dir)):
                 for fname in sorted(files):
                     if fname.endswith(".jpg"):
-                        if name == 'glaucoma':
-                            path = os.path.join(root, fname)
-                            item = (path, idx)
-                            self.data.append(item)
-        # self.labels_dict = {i: classes[i] for i in range(len(classes))}
-        self.labels_dict = {0: 'glaucoma'}
+                        # if name == 'glaucoma':
+                        #     path = os.path.join(root, fname)
+                        #     item = (path, idx)
+                        #     self.data.append(item)
+                        path = os.path.join(root, fname)
+                        item = (path, idx)
+                        self.data.append(item)
+        # self.labels_dict = {0: 'glaucoma'}
+        self.labels_dict = {i: classes[i] for i in range(len(classes))}
         
     def __len__(self):
         return len(self.data)
